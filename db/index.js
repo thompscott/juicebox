6e86ca3e510082061352e521baa77b4f2546c54c
yeisson giraldo <yeisonnn@gmail.com>
2022-07-15 14:03:26 -0500

    starting part1.2

--- db/index.js
+++ db/index.js
@@ -3,24 +3,53 @@ const { Client } = require('pg');
 const client = new Client('postgres://localhost:5432/juicebox-dev');
 
 async function getAllUsers() {
-  const { rows } = await client.query(`SELECT id, username 
+  const { rows } =
+    await client.query(`SELECT id, username, name, location, active 
       FROM users;
     `);
   return rows;
 }
 
-async function createUser( {username, password} ) {
+async function createUser({ username, password, name, location }) {
   try {
     const { rows } = await client.query(
       `
-    INSERT INTO users(username, password) VALUES ($1, $2)
+    INSERT INTO users(username, password, name, location) VALUES ($1, $2, $3, $4)
     ON CONFLICT (username) DO NOTHING
     RETURNING *;
   `,
-      [username, password]
+      [username, password, name, location]
     );
 
     return rows;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function updateUser(id, fields = {}) {
+  // build the set string
+  const setString = Object.keys(fields)
+    .map((key, index) => `"${key}"=$${index + 1}`)
+    .join(', ');
+
+  // return early if this is called without fields
+  if (setString.length === 0) {
+    return;
+  }
+
+  try {
+    const result = await client.query(
+      `
+        UPDATE users
+        SET ${setString}
+        WHERE id=${id}
+        RETURNING *;
+      `,
+      Object.values(fields)
+    );
+
+    return result;
   } catch (error) {
     throw error;
   }
@@ -30,4 +59,5 @@ module.exports = {
   client,
   getAllUsers,
   createUser,
+  updateUser,
 };
--- db/seed.js
+++ db/seed.js
@@ -1,4 +1,4 @@
-const { client, getAllUsers, createUser } = require('./index');
+const { client, getAllUsers, createUser, updateUser } = require('./index');
 
 async function testDB() {
   try {
@@ -39,7 +39,10 @@ async function createTables() {
         CREATE TABLE users (
           id SERIAL PRIMARY KEY,
           username varchar(255) UNIQUE NOT NULL,
-          password varchar(255) NOT NULL
+          password varchar(255) NOT NULL,
+          name VARCHAR(255) NOT NULL,
+          location VARCHAR(255) NOT NULL,
+          active BOOLEAN DEFAULT true
         );
       `);
 
@@ -54,19 +57,26 @@ async function createInitialUsers() {
   try {
     console.log('Starting to create users...');
 
-    const albert = await createUser({
+    await createUser({
       username: 'albert',
-      password: 'jose99',
+      password: 'bertie99',
+      name: 'Al Bert',
+      location: 'Sidney, Australia',
+      active: true,
     });
-
-    const sandra = await createUser({
-        username: 'sandra',
-        password: '2sandy4me'
+    await createUser({
+      username: 'sandra',
+      password: '2sandy4me',
+      name: 'Just Sandra',
+      location: "Ain't tellin'",
+      active: true,
     });
-
-    const glamgal = await createUser({
-        username: 'glamgal',
-        password: 'soglam'
+    await createUser({
+      username: 'glamgal',
+      password: 'soglam',
+      name: 'Joshua',
+      location: 'Upper East Side',
+      active: true,
     });
 
     console.log('Finished creating users!');
@@ -88,12 +98,34 @@ async function rebuildDB() {
   }
 }
 
+// async function testDB() {
+//   try {
+//     console.log('Starting to test database...');
+
+//     const users = await getAllUsers();
+//     console.log('getAllUsers:', users);
+
+//     console.log('Finished database tests!');
+//   } catch (error) {
+//     console.error('Error testing database!');
+//     throw error;
+//   }
+// }
+
 async function testDB() {
   try {
     console.log('Starting to test database...');
 
+    console.log('Calling getAllUsers');
     const users = await getAllUsers();
-    console.log('getAllUsers:', users);
+    console.log('Result:', users);
+
+    console.log('Calling updateUser on users[0]');
+    const updateUserResult = await updateUser(users[0].id, {
+      name: 'Newname Sogood',
+      location: 'Lesterville, KY',
+    });
+    console.log('Result:', updateUserResult);
 
     console.log('Finished database tests!');
   } catch (error) {
