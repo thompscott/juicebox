db9d90dc28f076adc1e7a51ef1d8d8c255496e6e
thompscott <tho.sco.lisd@gmail.com>
2022-07-20 11:08:05 -0500

    create post works, need to get current username

--- api/posts.js
+++ api/posts.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const postsRouter = express.Router();
-const { getAllPosts } = require('../db');
+const { getAllPosts, createPost, getUserByUsername } = require('../db');
+const { requireUser } = require('./utils');
+
+
 
 postsRouter.use((req, res, next) => {
   console.log('A request is being made to /posts');
@@ -14,6 +17,38 @@ postsRouter.get('/', async (req, res) => {
   res.send({
     posts
   });
+});
+
+postsRouter.post('/', requireUser, async (req, res, next) => {
+  const { title, content, tags = "" } = req.body;
+
+  const tagArr = tags.trim().split(/\s+/)
+  const postData = {};
+
+ 
+
+  // only send the tags if there are some to send
+  
+  postData.authorId = 1;
+  postData.title = title;
+  postData.content = content;
+  if (tagArr.length) {
+    postData.tags = tagArr;
+  }
+  
+
+
+  try {
+    const post = await createPost(postData)
+    res.send({ post });
+    // add authorId, title, content to postData object
+    // const post = await createPost(postData);
+    // this will create the post and the tags for us
+    // if the post comes back, res.send({ post });
+    // otherwise, next an appropriate error object 
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
 });
 
 module.exports = postsRouter;
--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 //1)MIDDLEWARES
 server.use(express.json());
 
-server.use((req, res, next) => {
+server.use( (req, res, next) => {
   console.log('<____Body Logger START____>');
   console.log(req.body);
   console.log('<_____Body Logger END_____>');
@@ -21,7 +21,7 @@ server.use('/api', apiRouter);
 //2)Routes
 
 // SERVER
-const { client } = require('./db');
+const { client, getUserByUsername } = require('./db');
 client.connect();
 
 server.listen(port, () => {
