ed338f33876375e49c4c6185a4006c3bfdab6057
thompscott <tho.sco.lisd@gmail.com>
2022-07-21 10:58:37 -0500

    Fixed comment

--- db/index.js
+++ db/index.js
@@ -2,7 +2,7 @@ const { Client } = require('pg');
 
 const client = new Client(process.env.DATABASE_URL || 'postgres://localhost:5432/juicebox-dev');
 
-/*user*/
+/*User*/
 async function createUser({ username, password, name, location }) {
   try {
     const { rows } = await client.query(
